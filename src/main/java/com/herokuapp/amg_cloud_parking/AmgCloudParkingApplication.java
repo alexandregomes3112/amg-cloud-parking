6852cc1151b04e8c714514126fd09d2f7a9053e4
Alexandre Gomes <alexandre.gomes.3112@gmail.com>
2025-01-06 15:11:38 -0300

    First Commit

---
+++ src/main/java/com/herokuapp/amg_cloud_parking/AmgCloudParkingApplication.java
@@ -0,0 +1,13 @@
+package com.herokuapp.amg_cloud_parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmgCloudParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmgCloudParkingApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/herokuapp/amg_cloud_parking/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.herokuapp.amg_cloud_parking.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "Hello There !<br>General Kenobi !";
+    }
+
+}
